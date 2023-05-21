8da043596b091ebe97f4c523e31bb40e6651f6f4
Victor Feliciano <victorjfeliciano@gmail.com>
2023-05-21 17:18:30 -0300

    applying highlight styles

--- components/Hightlight/Highlight.jsx
+++ components/Hightlight/Highlight.jsx
@@ -1,12 +1,23 @@
 import './Highlight.css'
+import Highlight1 from '@/public/assets/highlights/highlight1.jpg'
+import Highlight2 from '@/public/assets/highlights/highlight2.jpg'
+import Highlight3 from '@/public/assets/highlights/highlight3.jpg'
+import Image from 'next/image'
+
+
+const highlightMock = [
+    { id: 1, img: Highlight1 },
+    { id: 2, img: Highlight2 },
+    { id: 3, img: Highlight3 }
+]
 
 export default function Highlight() {
     return (
         <div className='Highlight'>
             <div className='itens'>
-                <div>Element 1</div>
-                <div>Element 2</div>
-                <div>Element 3</div>
+                {highlightMock.map(item => (
+                    <Image key={item.id} src={item.img} />
+                ))}
             </div>
             <div className='fixed-element'>
 
