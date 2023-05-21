1b51721a2100091976adca702a390c46b9312fae
Victor Feliciano <victorjfeliciano@gmail.com>
2023-05-21 14:38:15 -0300

    categories and footer skeletons

--- components/Categories/Categories.jsx
+++ components/Categories/Categories.jsx
@@ -1,7 +1,59 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
+import Placeholder from '@/public/assets/placeholder.jpg'
+import Image from 'next/image'
+import './Categories.css'
 
+const categoryMock = [
+    { id: 1, name: 'Eletrônicos' },
+    { id: 2, name: 'Cozinha' },
+    { id: 3, name: 'Lar' },
+    { id: 4, name: 'Roupas' },
+    { id: 5, name: 'Sapatos' },
+    { id: 6, name: 'Esporte' },
+]
 export default function Categories() {
+    const [page, setPage] = useState(0)
+
+
+    const handlePageChange = (change) => {
+        switch (change) {
+            case '-':
+                setPage((prevPage) => {
+                    if (prevPage == 0) {
+                        return prevPage
+                    }
+                    return prevPage + 1
+                })
+                break;
+            case '+':
+                setPage((prevPage) => {
+                    if (prevPage === (categoryMock.length - 3) * -1) {
+                        return prevPage
+                    }
+                    return prevPage - 1
+                })
+                break
+        }
+    }
+
     return (
-        <div>Categories</div>
+        <div className='Categories'>
+            <div className="carousel" style={{ transform: `translateX(${page * 32}vw)` }} >
+                {
+                    categoryMock.map(category => (
+                        <div key={category.id}>
+                            <div key={`${category.id}-img`} className='img-div'>
+                                <Image src={Placeholder} alt={category.name} />
+                            </div>
+                        </div>
+                    ))
+                }
+            </div>
+            <div className="buttons">
+                <button onClick={() => handlePageChange('-')}>{'<'}</button>
+                <button onClick={() => handlePageChange('+')}>{'>'}</button>
+            </div>
+        </div >
     )
 }
--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,7 +1,52 @@
-import React from 'react'
+import Image from 'next/image'
+import './Footer.css'
+import Facebook from '@/public/assets/social-media-icons/facebook.png'
+import Instagram from '@/public/assets/social-media-icons/instagram.png'
+import Twitter from '@/public/assets/social-media-icons/twitter.png'
+import Whatsapp from '@/public/assets/social-media-icons/whatsapp.png'
 
 export default function Footer() {
-    return (
-        <div>Footer</div>
-    )
+  return (
+    <footer className='Footer'>
+      <div className='social-media'>
+        <Image src={Facebook} alt='facebook' width={'30'} height={'30'} />
+        <Image src={Instagram} alt='instagram' width={'30'} height={'30'} />
+        <Image src={Twitter} alt='twitter' width={'30'} height={'30'} />
+        <Image src={Whatsapp} alt='whatsapp' width={'30'} height={'30'} />
+      </div>
+      <div className='store-info'>
+        <ul>
+          <li>Lorem ipsum dolor sit amet.</li>
+          <li>Lorem ipsum dolor sit amet.</li>
+          <li>Lorem ipsum dolor sit amet.</li>
+          <li>Lorem ipsum dolor sit amet.</li>
+          <li>Lorem ipsum dolor sit amet.</li>
+        </ul>
+        <ul>
+          <li>Lorem ipsum dolor sit amet.</li>
+          <li>Lorem ipsum dolor sit amet.</li>
+          <li>Lorem ipsum dolor sit amet.</li>
+          <li>Lorem ipsum dolor sit amet.</li>
+          <li>Lorem ipsum dolor sit amet.</li>
+        </ul>
+        <ul>
+          <li>Lorem ipsum dolor sit amet.</li>
+          <li>Lorem ipsum dolor sit amet.</li>
+          <li>Lorem ipsum dolor sit amet.</li>
+          <li>Lorem ipsum dolor sit amet.</li>
+          <li>Lorem ipsum dolor sit amet.</li>
+        </ul>
+      </div>
+
+      <div>
+        <button className='top-bt'>Voltar para o topo</button>
+      </div>
+
+      <div className='legal-info'>
+        <p>CNPJ 12.345.678/0000001-23</p>
+        <p>Av. Nextjs, JavaScrip - ESMAScript@2023 - Rio de Janeiro, Brasil</p>
+        <p>faleconosco@ajuda.com.br</p>
+      </div>
+    </footer>
+  )
 }
